97dc6159c93cc61f039b254bf33fc1ae4ae2449f
Elliot Glaze <elliotglaze98@gmail.com>
2020-06-25 10:12:48 +0100

    Create REST Web API

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 import ssl
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-def news(xml_news_url, counter):
+# GLOBAL VARIABLES
+data_news = {'articles': []}
+url = "https://news.google.com/news/rss/?ned=us&gl=US&hl=en"
+
+def news(xml_news_url, counter=20):
     context = ssl._create_unverified_context()
     response = urlopen(xml_news_url, context=context)
     xml_page = response.read()
@@ -14,7 +19,17 @@ def news(xml_news_url, counter):
     soup_page = soup(xml_page, 'xml')
     news_list = soup_page.find_all('item', limit=counter)
 
+    for article in news_list:
+        art_dict = {'title': article.title.text, 'pubDate': article.pubDate.text, 'link': article.link.text}
+        data_news['articles'].append(art_dict)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
-    return 'Hello, world'
+    news(url, 3)
+    return "<h1>API</h1><p>This is going to be where my api data sits</p>"
+
+@app.route('/api/articles/all', methods=['GET'])
+def api_all():
+    news(url)
+    return jsonify(data_news)
